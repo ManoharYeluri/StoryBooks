01baad8085c614237a944eebae02b744bc621935
Manohar Yeluri <manoharyeluri@users.noreply.github.com>
2020-10-07 01:40:26 +0530

    First Commit
    StoryBooks Project
    
    Node.js App From Scratch | Express, MongoDB & Google OAuth
    
    https://youtu.be/SBvmnHTQIPY

---
+++ app.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require("./config/db");
+
+//Load Config
+dotenv.config({ path: './config/config.env' });
+
+// Passport coonfig
+require('./config/passport')(passport)
+
+connectDB();
+
+const app = express()
+
+// Body parser
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json())
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars Helpers
+const { formatDate, stripTags, truncate } = require('./helper/hbs')
+
+//Handlebars
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate
+    }, defaultLayout: 'main', extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Session 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+})
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
